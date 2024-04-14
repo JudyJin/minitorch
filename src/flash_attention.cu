8f023c0507c7f02aa74faf9c20b7b8b39d3efa6c
jeferay <zhiruiw@andrew.cmu.edu>
2024-04-14 15:17:39 -0400

    Bug fixed: Put sync outside the loop Feature: Put m_ij, l_ij also into shared_mem for later use Feature: compute the shared_o then write back to global O 	The shared_o is computed in-place without overwritting shared_v 	bc threads take care of one row

--- src/flash_attention.cu
+++ src/flash_attention.cu
@@ -13,6 +13,7 @@
 namespace flash_attn{
 namespace cuda{
 
+// remember that br/bc might divide seq_len
 
 template <typename T>
 __global__ void flash_attn_fw(const T *Q, const T* K, const T* V, T* O, T* L, T* M, int seq_len, int head_dim,const T * masks, bool is_causal) {
@@ -42,7 +43,11 @@ __global__ void flash_attn_fw(const T *Q, const T* K, const T* V, T* O, T* L, T*
     __shared__ T shared_q[br][head_dim];
     __shared__ T shared_o[br][head_dim];
     __shared__ T shared_l[br];
+    __shared__ T shared_l_ij[br];
+    __shared__ T shared_l_new[br];
     __shared__ T shared_m[br];
+    __shared__ T shared_m_ij[br];
+    __shared__ T shared_m_new[br];
     __shared__ T shared_s[br][bc];
     
     // __shared__ T shared_q[br][head_dim];
@@ -50,25 +55,31 @@ __global__ void flash_attn_fw(const T *Q, const T* K, const T* V, T* O, T* L, T*
     for (int j=0;j<outer_steps;++j){
         // load KV to on-chip memory
         int kv_per_thread = (head_dim + br -1) / br;
-        for (int num = 0; num < kv_per_thread; ++num){
-            int ele_idx = threadIdx.y * num_per_thread + num;
-            shared_k[threadIdx.x][ele_idx] = K[batch_id * stride_batch + head_id * stride_head + (j * bc + threadIdx.x) * stride_seq + ele_idx];
-            shared_v[threadIdx.x][ele_idx] = V[batch_id * stride_batch + head_id * stride_head + (j * bc + threadIdx.x) * stride_seq + ele_idx];
+        for (int col_idx = 0; col_idx < kv_per_thread; ++col_idx){
+            int ele_idx = threadIdx.y * num_per_thread + col_idx;
+            if (ele_idx < head_dim){
+                shared_k[threadIdx.x][ele_idx] = K[batch_id * stride_batch + head_id * stride_head + (j * bc + threadIdx.x) * stride_seq + ele_idx];
+                shared_v[threadIdx.x][ele_idx] = V[batch_id * stride_batch + head_id * stride_head + (j * bc + threadIdx.x) * stride_seq + ele_idx];
+            }
         }
         __syncthreads();
         // inner loop
         for (int i = 0;i<inner_steps;++i){
-            //l load Q to on-chip memory
-            int q_per_thread = (head_dim + br -1) / br;
-            for (int num = 0; num < q_per_thread; ++num){
-                int ele_idx = threadIdx.x * num_per_thread + num;
-                shared_q[threadIdx.y][ele_idx] = Q[batch_id * stride_batch + head_id * stride_head + (i * br + threadIdx.y) * stride_seq + ele_idx];
-                if (threadIdx.x == 0){
-                    shared_l[threadIdx.y] = L[batch_id * n_head * seq_len + head_id * seq_len + (i * br + threadIdx.y)]; 
-                    shared_m[threadIdx.y] = M[batch_id * n_head * seq_len + head_id * seq_len + (i * br + threadIdx.y)];
+            //load Q to on-chip memory
+            int qo_per_thread = (head_dim + br -1) / br;
+            for (int col_idx = 0; col_idx < qo_per_thread; ++col_idx){
+                int ele_idx = threadIdx.x * num_per_thread + col_idx;
+                if (ele_idx < head_dim){
+                    shared_q[threadIdx.y][ele_idx] = Q[batch_id * stride_batch + head_id * stride_head + (i * br + threadIdx.y) * stride_seq + ele_idx];
                 }
-                __syncthreads();
             }
+            // always true for threadIdx.y < bc
+            // load l and m to on-chip memory
+            if (threadIdx.x == 0){
+                shared_l[threadIdx.y] = L[batch_id * n_head * seq_len + head_id * seq_len + (i * br + threadIdx.y)]; 
+                shared_m[threadIdx.y] = M[batch_id * n_head * seq_len + head_id * seq_len + (i * br + threadIdx.y)];
+            }
+            __syncthreads();
             // compute attention
             T sum_ = 0;
             for (int k = 0; k < head_dim; ++k){
@@ -77,33 +88,68 @@ __global__ void flash_attn_fw(const T *Q, const T* K, const T* V, T* O, T* L, T*
             shared_s[threadIdx.y][threadIdx.x] = sum_;
             
             __syncthreads();
-            // row max
+
+            // row-wise computation for the normalization factors
             if (threadIdx.x == 0){
-                T m_ij = 0;
+                T m_ij_ = 0;
                 for (int k = 0; k < bc; ++k){
-                    m_ij = max(m_ij, shared_s[threadIdx.y][k]);
+                    m_ij_ = max(m_ij_, shared_s[threadIdx.y][k]);
                 }
+                shared_m_ij[threadIdx.y] = mij_;
                 // softmax
-                T l_ij = 0;
+                T l_ij_ = 0;
                 for (int k = 0; k < bc; ++k){
-                    shared_s[threadIdx.y][k] = exp(shared_s[threadIdx.y][k] - m_ij);
-                    l_ij += shared_s[threadIdx.y][k];
+                    shared_s[threadIdx.y][k] = __expf(shared_s[threadIdx.y][k] - m_ij_);
+                    l_ij_ += shared_s[threadIdx.y][k];
                 }
+                shared_l_ij[threadIdx.y] = l_ij_;
+
                 // compute l and m
-                T m_new =  max(shared_m[threadIdx.y], m_ij);
-                T l_new = exp(shared_m[threadIdx.y] - m_new) * shared_l[threadIdx.y] + exp(m_ij - m_new) * l_ij;
-                // shared_l[threadIdx.y] = l_new;
-                // shared_m[threadIdx.y] = m_new;
+                T m_new_ = max(shared_m[threadIdx.y], m_ij_);
+                T l_new_ = __expf(shared_m[threadIdx.y] - m_new_) * shared_l[threadIdx.y] + __expf(m_ij_ - m_new_) * l_ij_;
+                
+                // need to write back to shared_memory for row sharing
+                shared_l_new[threadIdx.y] = l_new_;
+                shared_m_new[threadIdx.y] = m_new_;
             }
             __syncthreads();
-            // compute O
+
+            /*
+            Compute O on-chip and write back to HBM
+            */
+            // step 1: compute the shared_o
+            T factor_o = shared_l[threadIdx.y] * __expf(shared_m[threadIdx.y] - shared_m_new[threadIdx.y]) / shared_l_new[threadIdx.y];
+            T factor_pv = __expf(shared_m_ij[threadIdx.y] - shared_m_new[threadIdx.y]) / shared_l_new[threadIdx.y];
+            for (int col_idx = 0;col_idx<qo_per_thread; ++col_idx){
+                int ele_idx = threadIdx.x * num_per_thread + col_idx;
+                if (ele_idx < head_dim){
+                    T sum_pv = 0;
+                    for (int k = 0; k<bc; ++k){
+                        sum_pv += shared_s[threadIdx.y][k] * shared_v[k][ele_idx];
+                    }
+                    sum_pv *= factor_pv;
+                    T sum_o = shared_o[threadIdx.y][ele_idx] * factor_o + sum_pv;
+                    shared_o[threadIdx.y][ele_idx] = sum_o;
+                }
+            }
+            __syncthreads();
+
+            // step 2: write back to global O
+            for (int col_idx = 0; col_idx < qo_per_thread; ++col_idx){
+                int ele_idx = threadIdx.x * num_per_thread + col_idx;
+                if (ele_idx < head_dim){
+                    O[batch_id * stride_batch + head_id * stride_head * (i * br + threadIdx.y) * stride_seq + ele_idx] = shared_o[threadIdx.y][ele_idx];
+                }
+            }
+            if (threadIdx.x==0){
+                shared_l[threadIdx.y] = shared_l_new[threadIdx.y];
+                shared_m[threadIdx.y] = shared_m_new[threadIdx.y];
+            }
+            __syncthreads();
 
         }
 
-        
-
     }
-
 
 }
     
