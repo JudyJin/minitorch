d750b9ed0a5936b67833cd11cd8a45a10eea0fa3
jialusui1102 <jialusui1102@gmail.com>
2024-04-28 22:11:00 -0400

    update test file for mask

--- kernel_tests/test_flash_attn_bw.py
+++ kernel_tests/test_flash_attn_bw.py
@@ -100,11 +100,21 @@ def test_launch_flash_attn_bw():
   return custom, baseline
   # return custom
 
+
+def create_causal_mask(bs, nh, seq_len):
+    """
+    return a 1x1xTxt triangular causal mask for Q @ K^T (which will get broadcasted to BxHxTxT)
+    """
+    mask = -np.finfo(np.float32).max * np.triu(np.ones((1, 1, seq_len, seq_len), dtype=np.float32), 1) # This should be ok, but may be problematic
+    # mask = -np.finfo(datatype).max * np.triu(np.ones((bs, nh, seq_len, seq_len), dtype=datatype), 1) 
+    return minitorch.tensor_from_numpy(mask, backend=backend)
+
+
 @kt.case(atol=1e-3, rtol=1e-3, ntest=3)
 def test_launch_flash_attn_bw_mask():
   batch_size = 128
   nhead = 8
-  seq_len = 256
+  seq_len = 128
   head_dim = 32
   print(
       "(batch_size, nhead, seq_len, head_dim"
@@ -115,17 +125,17 @@ def test_launch_flash_attn_bw_mask():
   k = kt.rand((batch_size, nhead, seq_len, head_dim))
   v = kt.rand((batch_size, nhead, seq_len, head_dim))
   out_grad = kt.rand((batch_size, nhead, seq_len, head_dim))
-  mask = kt.dec_self_attn_mask(seq_len) * -1e8
   
   # Create minitorch tensors
   def custom():
     q_mt = minitorch.tensor(q.clone().tolist(), backend=backend, requires_grad=True)
     k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
     v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
-    mask_mt = minitorch.tensor(mask.clone().tolist(), backend=backend, requires_grad=True)
-
+    mask_mt = create_causal_mask(batch_size, nhead, seq_len)
     out_grad_mt = minitorch.tensor(out_grad.clone().tolist(), backend=backend, requires_grad=True)
+
     cust_out = q_mt.flash_attn(q_mt,k_mt,v_mt,mask_mt,minitorch.tensor_from_numpy(np.array(1)))
+
     # Compute gradients
     start_time = time.time()
     cust_out.backward(out_grad_mt)
@@ -137,18 +147,18 @@ def test_launch_flash_attn_bw_mask():
     # print("custom", q_grad)
     return [q_grad, k_grad, v_grad], end_time - start_time
   
-  def baseline():
+  def baseline_fuse():
     q_mt = minitorch.tensor(q.clone().tolist(), backend=backend, requires_grad=True)
     k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
     v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
+    mask_mt = create_causal_mask(batch_size, nhead, seq_len)
     out_grad_mt = minitorch.tensor(out_grad.clone().tolist(), backend=backend, requires_grad=True)
 
-    mask_ = mask.unsqueeze(0).unsqueeze(0)
-    mask_mt = minitorch.tensor(mask_.clone().tolist(), backend=backend, requires_grad=True)
-
+    # forward compute
     attn_score = q_mt@ k_mt.permute(0, 1, 3, 2) / (head_dim ** 0.5)
-    attn_score = attn_score.attn_softmax(mask_mt)
-    # attn_score = minitorch.nn.softmax(attn_score,dim=3)
+    attn_score += mask_mt
+    mask_zero = minitorch.zeros_tensor_from_numpy(shape=(batch_size,seq_len),backend=backend)
+    attn_score = attn_score.attn_softmax(mask_zero)
     bsl_out = attn_score@v_mt
 
     start_time = time.time()
@@ -167,11 +177,11 @@ def test_launch_flash_attn_bw_mask():
     k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
     v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
     out_grad_mt = minitorch.tensor(out_grad.clone().tolist(), backend=backend, requires_grad=True)
-    mask_mt = minitorch.tensor(mask.clone().tolist(), backend=backend, requires_grad=True)
+    mask_mt = create_causal_mask(batch_size, nhead, seq_len)
+
 
     attn_score = q_mt@ k_mt.permute(0, 1, 3, 2) / (head_dim ** 0.5)
     attn_score = minitorch.nn.softmax(attn_score + mask_mt, dim=3)
-    # attn_score = minitorch.nn.softmax(attn_score,dim=3)
     bsl_out = attn_score@v_mt
 
     start_time = time.time()
@@ -190,6 +200,5 @@ def test_launch_flash_attn_bw_mask():
     
 kt.init(device="cuda:0", nhead=8)
 kt.run(
-  # 'test_launch_flash_attn_bw',
   'test_launch_flash_attn_bw_mask'
 )
--- kernel_tests/test_flash_attn_fw.py
+++ kernel_tests/test_flash_attn_fw.py
@@ -14,72 +14,20 @@ import minitorch
 from minitorch.cuda_kernel_ops import CudaKernelOps
 backend = minitorch.TensorBackend(CudaKernelOps)
 
+def create_causal_mask(bs, nh, seq_len):
+    """
+    return a 1x1xTxt triangular causal mask for Q @ K^T (which will get broadcasted to BxHxTxT)
+    """
+    mask = -np.finfo(np.float32).max * np.triu(np.ones((1, 1, seq_len, seq_len), dtype=np.float32), 1) # This should be ok, but may be problematic
+    # mask = -np.finfo(datatype).max * np.triu(np.ones((bs, nh, seq_len, seq_len), dtype=datatype), 1) 
+    return minitorch.tensor_from_numpy(mask, backend=backend)  
 
-@kt.case(atol=1e-3, rtol=1e-3, ntest=5)
-def test_launch_flash_attn():
-  batch_size = 128
-  nhead = 32
-  seq_len = 300
-  head_dim = 16
-  print(
-      "(batch_size, nhead, seq_len, head_dim"
-      f"): ({batch_size}, {nhead}, {seq_len},{head_dim})"
-  )
-  q = kt.rand((batch_size, nhead, seq_len, head_dim))
-  k = kt.rand((batch_size, nhead, seq_len, head_dim))
-  v = kt.rand((batch_size, nhead, seq_len, head_dim))
-  mask_zero = kt.zeros((batch_size, nhead, seq_len,seq_len))
-  mask = kt.dec_self_attn_mask(seq_len) * -1e8
-  
-  # print("============q is===========\n",q[:,:,:,:10])
-
-  def custom():
-    q_mt = minitorch.tensor(q.clone().tolist(), backend=backend, requires_grad=True)
-    k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
-    v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
-    mask_mt = minitorch.tensor(mask.clone().tolist(), backend=backend, requires_grad=True)
-    
-
-    start_time = time.time()
-    cust_out = q_mt.flash_attn(q_mt,k_mt,v_mt,mask_mt,minitorch.tensor_from_numpy(np.array(0)))
-    end_time = time.time()
-
-    cust_out = torch.tensor(cust_out._tensor._storage).float().cuda()
-    return [
-        cust_out,
-    ], end_time - start_time
-
-  def baseline():
-    q_mt = minitorch.tensor(q.clone().tolist(), backend=backend, requires_grad=True)
-    k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
-    v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
-    
-    mask_ = mask.unsqueeze(0).unsqueeze(0)
-    mask_mt = minitorch.tensor(mask_.clone().tolist(), backend=backend, requires_grad=True)
-    start_time = time.time()
-    attn_score = q_mt@ k_mt.permute(0, 1, 3, 2) / (head_dim ** 0.5)
-    # mask = minitorch.zeros_tensor_from_numpy(shape=(batch_size,seq_len),backend=backend)
-    # attn_score = attn_score.attn_softmax(mask)
-    # attn_score = minitorch.nn.softmax(attn_score + mask_mt, dim=3)
-    attn_score = minitorch.nn.softmax(attn_score,dim=3)
-    bsl_out = attn_score@v_mt
-    # if not is_dec_self_attn_infer:
-    #   res = minitorch.nn.softmax(inp_mt + mask_mt, dim=3)
-    # else:
-    #   res = minitorch.nn.softmax(inp_mt, dim=3)
-    end_time = time.time()
-
-    bsl_out = torch.tensor(bsl_out._tensor._storage).float().cuda()
-    return kt.norm_res_list(bsl_out), end_time - start_time
-
-  return custom, baseline
-
-@kt.case(atol=1e-3, rtol=1e-3, ntest=5)
+@kt.case(atol=1e-3, rtol=1e-3, ntest=3)
 def test_launch_flash_attn_mask():
   batch_size = 128
-  nhead = 32
-  seq_len = 300
-  head_dim = 16
+  nhead = 8
+  seq_len = 256
+  head_dim = 32
   print(
       "(batch_size, nhead, seq_len, head_dim"
       f"): ({batch_size}, {nhead}, {seq_len},{head_dim})"
@@ -87,8 +35,6 @@ def test_launch_flash_attn_mask():
   q = kt.rand((batch_size, nhead, seq_len, head_dim))
   k = kt.rand((batch_size, nhead, seq_len, head_dim))
   v = kt.rand((batch_size, nhead, seq_len, head_dim))
-  mask_zero = kt.zeros((batch_size, nhead, seq_len,seq_len))
-  mask = kt.dec_self_attn_mask(seq_len) * -1e8
   
   # print("============q is===========\n",q[:,:,:,:10])
 
@@ -96,9 +42,8 @@ def test_launch_flash_attn_mask():
     q_mt = minitorch.tensor(q.clone().tolist(), backend=backend, requires_grad=True)
     k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
     v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
-    mask_mt = minitorch.tensor(mask.clone().tolist(), backend=backend, requires_grad=True)
+    mask_mt =create_causal_mask(batch_size, nhead, seq_len)
     
-
     start_time = time.time()
     cust_out = q_mt.flash_attn(q_mt,k_mt,v_mt,mask_mt,minitorch.tensor_from_numpy(np.array(1)))
     end_time = time.time()
@@ -108,35 +53,45 @@ def test_launch_flash_attn_mask():
         cust_out,
     ], end_time - start_time
 
-  def baseline():
+  def baseline_fuse():
     q_mt = minitorch.tensor(q.clone().tolist(), backend=backend, requires_grad=True)
     k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
     v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
-    
-    mask_ = mask.unsqueeze(0).unsqueeze(0)
-    mask_mt = minitorch.tensor(mask_.clone().tolist(), backend=backend, requires_grad=True)
+    mask_mt = create_causal_mask(batch_size, nhead, seq_len)
+
     start_time = time.time()
     attn_score = q_mt@ k_mt.permute(0, 1, 3, 2) / (head_dim ** 0.5)
-    # mask = minitorch.zeros_tensor_from_numpy(shape=(batch_size,seq_len),backend=backend)
-    # attn_score = attn_score.attn_softmax(mask)
-    attn_score = minitorch.nn.softmax(attn_score + mask_mt, dim=3)
-    # attn_score = minitorch.nn.softmax(attn_score,dim=3)
+    attn_score += mask_mt
+    mask_zero = minitorch.zeros_tensor_from_numpy(shape=(batch_size,seq_len),backend=backend)
+    attn_score = attn_score.attn_softmax(mask_zero)
     bsl_out = attn_score@v_mt
-    # if not is_dec_self_attn_infer:
-    #   res = minitorch.nn.softmax(inp_mt + mask_mt, dim=3)
-    # else:
-    #   res = minitorch.nn.softmax(inp_mt, dim=3)
     end_time = time.time()
 
     bsl_out = torch.tensor(bsl_out._tensor._storage).float().cuda()
     return kt.norm_res_list(bsl_out), end_time - start_time
+  
+  def baseline_no_fuse():
+    q_mt = minitorch.tensor(q.clone().tolist(), backend=backend, requires_grad=True)
+    k_mt = minitorch.tensor(k.clone().tolist(), backend=backend, requires_grad=True)
+    v_mt = minitorch.tensor(v.clone().tolist(), backend=backend, requires_grad=True)
+    mask_mt = create_causal_mask(batch_size, nhead, seq_len)
 
-  return custom, baseline
+    start_time = time.time()
+    attn_score = q_mt@ k_mt.permute(0, 1, 3, 2) / (head_dim ** 0.5)
+    attn_score += mask_mt
+    attn_score = minitorch.nn.softmax(attn_score, dim=3)
+    bsl_out = attn_score@v_mt
+    end_time = time.time()
+
+    bsl_out = torch.tensor(bsl_out._tensor._storage).float().cuda()
+    return kt.norm_res_list(bsl_out), end_time - start_time
+     
+
+  return baseline_fuse, baseline_no_fuse
 
 
 
 kt.init(device="cuda:0", nhead=8)
 kt.run(
-  'test_launch_flash_attn',
   'test_launch_flash_attn_mask'
 )
